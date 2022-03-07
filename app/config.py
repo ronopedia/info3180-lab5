c74acafb2b4d50f69b57ac754e5a1e505471e890
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-03-06 23:25:29 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,3 +9,12 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or 'postgresql://lab5:lab5@localhost/lab5'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
